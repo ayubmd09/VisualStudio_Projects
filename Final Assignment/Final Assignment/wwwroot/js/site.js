1f5d97104558d69c4f4bd01c7581b342d29cf061
ayubmd09 <ayubmohammed1439@gmail.com>
2026-06-03 16:25:10 -0400

    Final Assignment
    A basic HTML webpage with an embedded music video from Youtube of one of your favorite songs,
    
    Customize some element of the page with CSS (such as font or background color),
    
    Using JavaScript, create some form of animation on the webpage.

---
+++ Final Assignment/Final Assignment/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Function to animate the box when the button is clicked
+function moveBox() {
+
+    // Get the box element from the page
+    let box = document.getElementById("box");
+
+    // Starting position
+    let position = 0;
+
+    // Move the box repeatedly
+    let animation = setInterval(function () {
+
+        // Stop animation when box reaches 300px
+        if (position >= 300) {
+            clearInterval(animation);
+        } else {
+
+            // Move box one pixel at a time
+            position++;
+
+            // Update the box position
+            box.style.left = position + "px";
+        }
+    }, 5);// Speed of animation
+}
