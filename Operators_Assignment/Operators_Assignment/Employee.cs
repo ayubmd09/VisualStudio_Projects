635a0b32e97e71c1b36aaa2b33b2d0f767295cce
ayubmd09 <ayubmohammed1439@gmail.com>
2026-08-05 18:41:57 -0400

    Operators Assignment
    Operators Assignment

---
+++ Operators_Assignment/Operators_Assignment/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    internal class Employee
+    {
+        // Employee Id property.
+        public int Id { get; set; }
+
+        // Employee first name property.
+        public string FirstName { get; set; }
+
+        // Employee last name property.
+        public string LastName { get; set; }
+
+        // Overload the == operator.
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload the != operator.
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Id != emp2.Id;
+        }
+    }
+}
---
+++ Operators_Assignment/Operators_Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the first Employee object.
+            Employee emp1 = new Employee();
+            emp1.Id = 1;
+            emp1.FirstName = "Ayub";
+            emp1.LastName = "M";
+
+            // Create the second Employee object.
+            Employee emp2 = new Employee();
+            emp2.Id = 2;
+            emp2.FirstName = "Robert";
+            emp2.LastName = "Jr";
+
+            // Compare the two Employee objects using the overloaded == operator.
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("The employees are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The employees are not equal.");
+            }
+
+            // Keep the console window open.
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
